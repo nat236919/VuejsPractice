8e2c464dab346fac2d928e35ff1d92bcfa0592f5
nat236919 <nuttaphat.aruno@gmail.com>
2020-09-29 11:39:02 +0800

    create List Rendering

--- basic/static/js/main.js
+++ basic/static/js/main.js
@@ -12,7 +12,19 @@ var app = new Vue({
         image: "https://rohan.imgix.net/product/05706H08.jpg",
         product: "Socks",
         desc: "Who doesn't love to have a good pair of socks? Get yourself a treat :)",
-        inventory: 10,
+        details: ["80% cotton", "19% polyester", "1% love", "LGBT friendly"],
+        variants: [
+            {
+                variantId: 1,
+                variantColor: "black"
+            },
+            {
+                variantId: 2,
+                variantColor: "green"
+            }
+        ],
+        sizes: ["S", "M", "L", "XL"],
+        inventory: 11,
         onSale: true,
         link: "https://get.foundation/index.html"
     }
